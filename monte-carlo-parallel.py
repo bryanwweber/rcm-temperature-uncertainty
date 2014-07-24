b8491345aa7a35df78f1b2d9ebe36704bf37a4b9
Bryan W. Weber <bryan@bryanwweber.com>
2014-07-24 08:58:27 -0400

    Add comments to Monte Carlo code. Add volume distribution. Change variable names to be fuel generic

--- monte-carlo-parallel.py
+++ monte-carlo-parallel.py
@@ -10,33 +10,31 @@ import subprocess
 import os
 
 def run_case(n):
-    temperatures = np.arange(300,1105,5)
-    mch = ct.Solution('therm-data.xml')
-    mch_mw = mch.molecular_weights[mch.species_index('mch')]
-    mch.basis = 'molar'
-    mch_cp = np.zeros(len(temperatures))
-    Ti = 294.45
-    tank_volume = 0.017561
+    # Set the string of the fuel. Possible values with the distributed
+    # therm-data.xml are 'mch', 'nc4h9oh', 'sc4h9oh', 'tc4h9oh', 'ic4h9oh',
+    # 'ic5h11oh', and 'c3h6'
+    fuel = 'mch'
 
-    mass_fuel = 3.46
-    mole_fuel = mass_fuel/mch_mw
-    mole_o2 = mole_fuel*10.5
-    mole_n2 = mole_fuel*12.25
-    mole_ar = mole_fuel*71.75
+    # Set the Cantera Solution with the thermo data from the xml file.
+    # Get the molecular weight of the fuel and set the unit basis for the
+    # Solution to molar basis.
+    gas = ct.Solution('therm-data.xml')
+    fuel_mw = gas.molecular_weights[gas.species_index(fuel)]
+    gas.basis = 'molar'
 
-    nom_o2_pres = mole_o2*ct.gas_constant*Ti/(1000*tank_volume)
-    nom_n2_pres = mole_n2*ct.gas_constant*Ti/(1000*tank_volume)
-    nom_ar_pres = mole_ar*ct.gas_constant*Ti/(1000*tank_volume)
+    # Set the ambient temperature and tank volume
+    Ta = 21.7+273.15
+    # Convert the ambient temperature to °C to match the spec.
+    sigma_Ta = max(2.2,(Ta-273.15)*0.0075)/2
+    Ta_dist = norm_dist(loc=Ta, scale=sigma_Ta)
+    nom_tank_volume = 0.01660
+    sigma_volume = 0.00001
+    vol_dist = norm_dist(loc=nom_tank_volume, scale=sigma_volume)
 
-    sigma_pressure = 346.6/2
-    sigma_mass = 0.03
-    # Convert the initial temperature to °C to match the spec.
-    sigma_Ti = max(2.2,(Ti-273.15)*0.0075)/2
-
-    Ti_dist = norm_dist(loc=Ti, scale=sigma_Ti)
-    mass_dist = norm_dist(loc=mass_fuel, scale=sigma_mass)
-
-    T0 = 308
+    # Set the initial temperature (in K), initial pressure (in Pa), and
+    # compressed pressure (in Pa). Create the normal distributions for each
+    # of these parameters.
+    T0 = 295.1
     # Convert the initial temperature to °C to match the spec.
     sigma_T0 = max(2.2,(T0-273)*0.0075)/2
     T0_dist = norm_dist(loc=T0, scale=sigma_T0)
@@ -49,6 +47,30 @@ def run_case(n):
     sigma_PC = 5000/2
     PC_dist = norm_dist(loc=PC, scale=sigma_PC)
 
+    # Set the nominal injected mass of the fuel. Compute the nominal moles of
+    # fuel and corresponding nominal required number of moles of the gases.
+    nom_mass_fuel = 3.48
+    nom_mole_fuel = nom_mass_fuel/fuel_mw
+    nom_mole_o2 = nom_mole_fuel*10.5
+    nom_mole_n2 = nom_mole_fuel*12.25
+    nom_mole_ar = nom_mole_fuel*71.75
+
+    # Create the normal distribution for the fuel mass
+    sigma_mass = 0.04/2
+    fuel_mass_dist = norm_dist(loc=nom_mass_fuel, scale=sigma_mass)
+
+    # Calculate the nominal pressure required for each gas to match the desired
+    # molar proportions. Note that the gas constant from Cantera is given in
+    # units of J/kmol-K, hence the factor of 1000.
+    nom_o2_pres = nom_mole_o2*ct.gas_constant*Ta/(1000*nom_tank_volume)
+    nom_n2_pres = nom_mole_n2*ct.gas_constant*Ta/(1000*nom_tank_volume)
+    nom_ar_pres = nom_mole_ar*ct.gas_constant*Ta/(1000*nom_tank_volume)
+
+    # Compute the pressures of each component as they are filled into the
+    # mixing tank. The mean of the distribution of the pressure of each
+    # subsequent gas is the sum of the sampled value of the pressure of the
+    # previous gas plus the nominal value of the current gas.
+    sigma_pressure = 346.6/2
     o2_dist = norm_dist(loc=nom_o2_pres, scale=sigma_pressure)
     o2_pres_rand = o2_dist.ppf(np.random.random_sample())
     n2_pressure = o2_pres_rand + nom_n2_pres
@@ -57,33 +79,51 @@ def run_case(n):
     ar_pressure = n2_pres_rand + nom_ar_pres
     ar_dist = norm_dist(loc=ar_pressure, scale=sigma_pressure)
     ar_pres_rand = ar_dist.ppf(np.random.random_sample())
-    mole_fuel_rand = mass_dist.ppf(np.random.random_sample())/mch_mw
-    Ti_rand = Ti_dist.ppf(np.random.random_sample())
-    T0_rand = T0_dist.ppf(np.random.random_sample())
-    o2_pres_T0 = T0_rand*o2_pres_rand/Ti_rand
-    n2_pres_T0 = T0_rand*n2_pres_rand/Ti_rand
-    ar_pres_T0 = T0_rand*ar_pres_rand/Ti_rand
 
-    mole_o2_rand = o2_pres_T0*tank_volume*1000/(ct.gas_constant*Ti_rand)
-    mole_n2_rand = n2_pres_T0*tank_volume*1000/(ct.gas_constant*Ti_rand)
-    mole_ar_rand = ar_pres_T0*tank_volume*1000/(ct.gas_constant*Ti_rand)
+    # Sample random values of the ambient temperature, tank volume, and fuel
+    # mass from their distributions.
+    Ta_rand = Ta_dist.ppf(np.random.random_sample())
+    tank_volume_rand = vol_dist.ppf(np.random.random_sample())
+    mole_fuel_rand = fuel_mass_dist.ppf(np.random.random_sample())/fuel_mw
+
+    # Compute the number of moles of each gaseous component based on the
+    # sampling from the various distributions. Note that the gas constant from
+    # Cantera is given in units of J/kmol-K, hence the factor of 1000.
+    mole_o2_rand = o2_pres_rand*tank_volume_rand*1000/(ct.gas_constant*Ta_rand)
+    mole_n2_rand = n2_pres_rand*tank_volume_rand*1000/(ct.gas_constant*Ta_rand)
+    mole_ar_rand = ar_pres_rand*tank_volume_rand*1000/(ct.gas_constant*Ta_rand)
+
+    # Compute the mole fractions of each component and set the state of the
+    # Cantera solution.
     total_moles = sum([mole_fuel_rand, mole_o2_rand, mole_n2_rand, mole_ar_rand])
-    mole_fractions = 'mch:{mch},o2:{o2},n2:{n2},ar:{ar}'.format(
-        mch=mole_fuel_rand/total_moles, o2=mole_o2_rand/total_moles,
+    mole_fractions = '{fuel_name}:{fuel_mole},o2:{o2},n2:{n2},ar:{ar}'.format(
+        fuel_name=fuel, fuel_mole=mole_fuel_rand/total_moles, o2=mole_o2_rand/total_moles,
         n2=mole_n2_rand/total_moles, ar=mole_ar_rand/total_moles)
-    mch.TPX = None, None, mole_fractions
-    
-    for i, temp in enumerate(temperatures):
-        mch.TP = temp, None
-        mch_cp[i] = mch.cp/ct.gas_constant
+    gas.TPX = None, None, mole_fractions
 
-    (mch_b, mch_a) = np.polyfit(temperatures, mch_cp, 1)
+    # Initialize the array of temperatures over which the C_p should be fit.
+    # The range is [first input, second input) with increment set by the third
+    # input. Loop through the temperatures and compute the non-dimensional
+    # specific heats.
+    temperatures = np.arange(300,1105,5)
+    gas_cp = np.zeros(len(temperatures))
+    for i, temp in enumerate(temperatures):
+        gas.TP = temp, None
+        gas_cp[i] = gas.cp/ct.gas_constant
+
+    # Compute the linear fit to the specific heat.
+    (gas_b, gas_a) = np.polyfit(temperatures, gas_cp, 1)
+
+    # Sample the values for the initial temperature, initial pressure, and
+    # compressed pressure.
+    T0_rand = T0_dist.ppf(np.random.random_sample())
     P0_rand = P0_dist.ppf(np.random.random_sample())
     PC_rand = PC_dist.ppf(np.random.random_sample())
-    mch_lam_rand = mch_b/mch_a * np.exp(mch_b*T0_rand/mch_a) * T0_rand * (PC_rand/P0_rand)**(1/mch_a)
-    mch_TC_rand = np.real(mch_a * lambertw(mch_lam_rand)/mch_b)
-    return mch_TC_rand
 
+    # Compute the compressed temperature and return it.
+    lam_rand = gas_b/gas_a * np.exp(gas_b*T0_rand/gas_a) * T0_rand * (PC_rand/P0_rand)**(1/gas_a)
+    TC_rand = np.real(gas_a * lambertw(lam_rand)/gas_b)
+    return TC_rand
 
 if __name__ == "__main__":
     start = time.time()
